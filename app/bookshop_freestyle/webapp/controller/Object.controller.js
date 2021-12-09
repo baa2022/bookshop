b4bad785862c7235ad4579b965a45963059c393a
Alina Balysh <alina.balysh@leverx.com>
2021-12-09 08:52:25 +0000

    create fragments for edit mode for object page

--- app/bookshop_freestyle/webapp/controller/Object.controller.js
+++ app/bookshop_freestyle/webapp/controller/Object.controller.js
@@ -3,27 +3,22 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "sap/m/MessageBox",
     "sap/ui/model/Sorter",
-], function (BaseController, JSONModel, MessageBox, Sorter) {
+], function (BaseController, JSONModel) {
     "use strict";
 
-    return BaseController.extend("bs.bookstore.controller.Object", {
-        /* =========================================================== */
-        /* lifecycle methods                                           */
-        /* =========================================================== */
+    return BaseController.extend("bookshop.freestyle.bookshopfreestyle.controller.Object", {
 
-        /**
-         * Called when the booklist controller is instantiated.
-         * @public
-         */
         onInit: function () {
             const oViewModel = new JSONModel({
                 isEditingMode: false,
                 sortStateOrder: 0
             });
-            
 
             this.setModel(oViewModel, "objectView");
             this.getRouter().getRoute("object").attachPatternMatched(this.onObjectMatched, this);
+
+            this._oFragmentsMap = this.createFragmentsMap();
+            this.showFormFragment();
         },
 
         onObjectMatched: function (oEvent) {
@@ -42,6 +37,41 @@ sap.ui.define([
 
         onBookLinkPress: function () {
             this.navigateTo("worklist");
+        },
+
+        createFragmentsMap: function () {
+            const pEditFragment = this.loadFragment({
+                name: "bookshop.freestyle.bookshopfreestyle.view.fragments.EditBook"
+            });
+            const pDisplayFragment = this.loadFragment({
+                name: "bookshop.freestyle.bookshopfreestyle.view.fragments.DisplayBook"
+            });
+
+            const oFragmentsMap = new Map([
+                [true, pEditFragment],
+                [false, pDisplayFragment]
+            ]);
+
+            return oFragmentsMap;
+        },
+
+        showFormFragment: function () {
+            const isEditingMode = this.getModel("objectView").getProperty("/isEditingMode");
+            const oFormShell = this.byId("formShell");
+
+            this._oFragmentsMap.get(isEditingMode)
+                .then(function (oForm) {
+                    oFormShell.removeAllContent();
+                    oFormShell.addContent(oForm);
+                });
+        },
+
+        onEditBookPress: function () {
+            const oViewModel = this.getModel("objectView");
+            const isEditingMode = oViewModel.getProperty("/isEditingMode");
+
+            oViewModel.setProperty("/isEditingMode", !isEditingMode);
+            this.showFormFragment();
         },
 
     });
