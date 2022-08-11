658cb720e29c0e086ff9d915b105ce873be23af7
Alina Balysh <alina.balysh@leverx.com>
2022-08-11 08:34:49 +0000

    update deploy

---
+++ db/sql/BooksInsertQueries.sql
@@ -0,0 +1,164 @@
+INSERT INTO "DE0F593BE82B44FD99DD6F73C5CB50F4"."SAP_CAPIRE_BOOKSHOP_BOOKS" VALUES(
+'3b48fcc6-f5b9-4e16-9260-4bceaf8a50c3',
+26,
+23,
+'USD',
+'Wuthering Heights',
+'Wuthering Heights is an 1847 novel by Emily Brontë, initially published under the pseudonym Ellis Bell. It concerns two families of the landed gentry living on the West Yorkshire moors, the Earnshaws and the Lintons, and their turbulent relationships with Earnshaw''s adopted son, Heathcliff.',
+'Tragedy',
+'06f8349f-fb9e-41d0-9070-363910af4021',
+5)
+
+INSERT INTO "DE0F593BE82B44FD99DD6F73C5CB50F4"."SAP_CAPIRE_BOOKSHOP_BOOKS" VALUES(
+'4563beb9-31cb-4813-825b-700ef043c638',
+110,
+52,
+'USD',
+'Jane Eyre',
+'Jane Eyre is a novel by English writer Charlotte Brontë, published under the pen name "Currer Bell", on 16 October 1847, by Smith, Elder & Co. of London. The first American edition was published the following year by Harper & Brothers of New York.Jane Eyre is a novel by English writer Charlotte Brontë, published under the pen name "Currer Bell", on 16 October 1847, by Smith, Elder & Co. of London. The first American edition was published the following year by Harper & Brothers of New York.',
+'Romance',
+'07662818-79af-4289-b6ee-e5c79a9572d0',
+4)
+
+INSERT INTO "DE0F593BE82B44FD99DD6F73C5CB50F4"."SAP_CAPIRE_BOOKSHOP_BOOKS" VALUES(
+'6f37ccca-34dc-4453-89da-64ef04844c9d',
+59,
+32,
+'USD',
+'The Raven',
+'First published in January 1845, the poem is often noted for its musicality, stylized language, and supernatural atmosphere. It tells of a talking raven''s mysterious visit to a distraught lover, tracing the man''s slow descent into madness.',
+'Poetry',
+'7dbf2fec-17ed-4990-b636-5f4cc68fc546',
+5)
+
+INSERT INTO "DE0F593BE82B44FD99DD6F73C5CB50F4"."SAP_CAPIRE_BOOKSHOP_BOOKS" VALUES(
+'96edb013-84ce-4ad2-b0c5-a0d094c9ebc4',
+78,
+22,
+'USD',
+'Eleonora',
+'"Eleonora" is a short story by Edgar Allan Poe, first published in 1842 in Philadelphia in the literary annual The Gift. It is often regarded as somewhat autobiographical and has a relatively "happy" ending.',
+'Action',
+'7dbf2fec-17ed-4990-b636-5f4cc68fc546',
+3)
+
+INSERT INTO "DE0F593BE82B44FD99DD6F73C5CB50F4"."SAP_CAPIRE_BOOKSHOP_BOOKS" VALUES(
+'de6d0417-042a-49d8-81a8-18b6db28ac03',
+12,
+49,
+'USD',
+'Catweazle',
+'Catweazle is a magician from the time of the Normans who is cast into the future by magic. With the help of two boys he uses magic in an attempt to return to his own time.',
+'Comedy',
+'aa5be420-27be-492d-a000-4a772f5c2865',
+5)
+
+INSERT INTO "DE0F593BE82B44FD99DD6F73C5CB50F4"."SAP_CAPIRE_BOOKSHOP_BOOKS" VALUES(
+'d036b2c2-02b7-4c78-9549-43a5c6d2e371',
+993,
+53,
+'USD',
+'Robin of Sherwood: The Hooded Man',
+'In a dramatic retelling of the famous legend, a young boy dreams of Robin Hood, Sherwood Forest''s most notorious outlaw, who is discovering the dangers that go along with his fame and success.',
+'Drama',
+'aa5be420-27be-492d-a000-4a772f5c2865',
+4)
+
+INSERT INTO "DE0F593BE82B44FD99DD6F73C5CB50F4"."SAP_CAPIRE_BOOKSHOP_BOOKS" VALUES(
+'620bc15a-0990-4163-b071-9aaf191e8ca0',
+56,
+13,
+'USD',
+'Hamlet',
+'The Tragedy of Hamlet, Prince of Denmark, often shortened to Hamlet, is a tragedy written by William Shakespeare sometime between 1599 and 1601. It is Shakespeare''s longest play, with 29,551 words.',
+'Tragedy',
+'b68afdde-1231-4e80-9611-0fa8ad3eb1ea',
+5)
+
+INSERT INTO "DE0F593BE82B44FD99DD6F73C5CB50F4"."SAP_CAPIRE_BOOKSHOP_BOOKS" VALUES(
+'8185300f-47d7-45c7-b3db-0e7b5ca2afd1',
+166,
+24,
+'USD',
+'Romeo and Juliet',
+'Romeo and Juliet is a tragedy written by William Shakespeare early in his career about two young Italian star-crossed lovers whose deaths ultimately reconcile their feuding families. It was among Shakespeare''s most popular plays during his lifetime and, along with Hamlet, is one of his most frequently performed plays.',
+'Tragedy',
+'b68afdde-1231-4e80-9611-0fa8ad3eb1ea',
+4)
+
+INSERT INTO "DE0F593BE82B44FD99DD6F73C5CB50F4"."SAP_CAPIRE_BOOKSHOP_BOOKS" VALUES(
+'90f10eca-ef79-47fe-9bf1-706d71e26eeb',
+76,
+32,
+'USD',
+'Murder on the Orient Express',
+'Murder on the Orient Express is a work of detective fiction by English writer Agatha Christie featuring the Belgian detective Hercule Poirot. It was first published in the United Kingdom by the Collins Crime Club on 1 January 1934.',
+'Mystery',
+'bbb3b110-7363-492f-b294-f51a898dd06b',
+4)
+
+INSERT INTO "DE0F593BE82B44FD99DD6F73C5CB50F4"."SAP_CAPIRE_BOOKSHOP_BOOKS" VALUES(
+'29b9744d-d816-4310-807f-bb0a6f8aa6ab',
+98,
+11,
+'USD',
+'Complications',
+'Complications',
+'Romance',
+'fbab3bbc-efe0-4bbd-ab31-2663c3e7c197',
+3)
+
+INSERT INTO "DE0F593BE82B44FD99DD6F73C5CB50F4"."SAP_CAPIRE_BOOKSHOP_BOOKS" VALUES(
+'bec06fdb-9e28-48cd-adce-73de58e0ead1',
+225,
+14,
+'USD',
+'Harry Potter and the Philosopher''s Stone',
+'Harry Potter, an eleven-year-old orphan, discovers that he is a wizard and is invited to study at Hogwarts. Even as he escapes a dreary life and enters a world of magic, he finds trouble awaiting him.',
+'Fantasy',
+'d6cb40fb-659c-4b91-9079-b55afdaf0833',
+5)
+
+INSERT INTO "DE0F593BE82B44FD99DD6F73C5CB50F4"."SAP_CAPIRE_BOOKSHOP_BOOKS" VALUES(
+'4408b8e9-9cd9-43c1-97ed-64b1d29e9a0c',
+476,
+39,
+'USD',
+'Harry Potter and the Chamber of Secrets',
+'A house-elf warns Harry against returning to Hogwarts, but he decides to ignore it. When students and creatures at the school begin to get petrified, Harry finds himself surrounded in mystery.',
+'Fantasy',
+'d6cb40fb-659c-4b91-9079-b55afdaf0833',
+5)
+
+INSERT INTO "DE0F593BE82B44FD99DD6F73C5CB50F4"."SAP_CAPIRE_BOOKSHOP_BOOKS" VALUES(
+'9fd4116a-616b-4265-938c-65ba4eb1e0f7',
+179,
+26,
+'USD',
+'Harry Potter and the Prisoner of Azkaban',
+'Harry, Ron and Hermoine return to Hogwarts just as they learn about Sirius Black and his plans to kill Harry. However, when Harry runs into him, he learns that the truth is far from reality.',
+'Fantasy',
+'d6cb40fb-659c-4b91-9079-b55afdaf0833',
+5)
+
+INSERT INTO "DE0F593BE82B44FD99DD6F73C5CB50F4"."SAP_CAPIRE_BOOKSHOP_BOOKS" VALUES(
+'f5afb9b1-490c-4dc3-833e-2e74a87e80ea',
+245,
+29,
+'USD',
+'Anna Karenina',
+'Anna Karenina is a novel by the Russian author Leo Tolstoy, first published in book form in 1878. Many writers consider it to be one of the greatest works of literature ever written, and Tolstoy himself called it his first true novel.',
+'Classic',
+'370776ea-d0bf-46dc-8e3e-d22dcc3ef140',
+5)
+
+INSERT INTO "DE0F593BE82B44FD99DD6F73C5CB50F4"."SAP_CAPIRE_BOOKSHOP_BOOKS" VALUES(
+'ce298336-2e1d-4fae-9f37-0c27942b2317',
+34,
+15,
+'USD',
+'War and Peace',
+'War and Peace is a literary work mixed with chapters on history and philosophy by the Russian author Leo Tolstoy, first published serially, then published in its entirety in 1869. It is regarded as one of Tolstoy''s finest literary achievements and remains an internationally praised classic of world literature.',
+'Classic',
+'370776ea-d0bf-46dc-8e3e-d22dcc3ef140',
+5)
